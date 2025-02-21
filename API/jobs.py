aa4aacd9245c0eb1a161c6f41171c6173535628f
joracab <jose.l.ramirez@gmail.com>
2025-02-20 23:14:42 -0500

    job fle updated

--- API/jobs.py
+++ API/jobs.py
@@ -14,65 +14,65 @@ db_config = {
 app = Flask(__name__)
 
 # get all depts
-@app.route("/departments", methods=["GET"])
-def get_departments():
+@app.route("/jobs, methods=["GET"])
+def get_jobs():
     try:
         with pymysql.connect(**db_config) as connection, connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM departments")
-            departments = cursor.fetchall()
-            return jsonify(departments)
+            cursor.execute("SELECT * FROM jobs")
+            jobs = cursor.fetchall()
+            return jsonify(jobs)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# Get dept by ID
-@app.route("/departments/<int:id>", methods=["GET"])
-def get_department(id):
+# Get job by ID
+@app.route("/jobs/<int:id>", methods=["GET"])
+def get_job(id):
     try:
         with pymysql.connect(**db_config) as connection, connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM departments WHERE id = %s", (id,))
-            department = cursor.fetchone()
-            if not department:
-                return jsonify({"error": "Department not found"}), 404
-            return jsonify(department)
+            cursor.execute("SELECT * FROM job WHERE id = %s", (id,))
+            job = cursor.fetchone()
+            if not job:
+                return jsonify({"error": "job not found"}), 404
+            return jsonify(job)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# Insert new dept
-@app.route("/departments", methods=["POST"])
-def create_department():
+# Insert new job
+@app.route("/jobs", methods=["POST"])
+def create_job():
     try:
         data = request.json
         
         with pymysql.connect(**db_config) as connection, connection.cursor() as cursor:
-            sql = "INSERT INTO departments (id, department) VALUES (%s, %s)"
-            cursor.execute(sql, (data['id'], data['department']))
+            sql = "INSERT INTO jobs (id, department) VALUES (%s, %s)"
+            cursor.execute(sql, (data['id'], data['job']))
             connection.commit()
-        return jsonify({"message": "Departamento created"})
+        return jsonify({"message": "job created"})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# Update department
-@app.route("/departments/<int:id>", methods=["PUT"])
-def update_department(id):
+# Update job
+@app.route("/jobs/<int:id>", methods=["PUT"])
+def update_job(id):
     try:
         data = request.json
         
         with pymysql.connect(**db_config) as connection, connection.cursor() as cursor:
-            sql = "UPDATE departments SET department=%s WHERE id=%s"
-            cursor.execute(sql, (data['department'], id))
+            sql = "UPDATE jobs SET job=%s WHERE id=%s"
+            cursor.execute(sql, (data['job'], id))
             connection.commit()
-        return jsonify({"message": "Department updated!"})
+        return jsonify({"message": "job updated!"})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# Delete department
-@app.route("/departments/<int:id>", methods=["DELETE"])
-def delete_department(id):
+# Delete job
+@app.route("/job/<int:id>", methods=["DELETE"])
+def delete_job(id):
     try:
         with pymysql.connect(**db_config) as connection, connection.cursor() as cursor:
-            cursor.execute("DELETE FROM departments WHERE id = %s", (id,))
+            cursor.execute("DELETE FROM jobs WHERE id = %s", (id,))
             connection.commit()
-        return jsonify({"message": "Department deleted!"})
+        return jsonify({"message": "job deleted!"})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
