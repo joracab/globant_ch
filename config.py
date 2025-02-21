773ef5c94fb072d7a7d72235a01e74971e73c2a5
joracab <jose.l.ramirez@gmail.com>
2025-02-21 18:03:40 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import pymysql
 class Config:
     DB_HOST = os.getenv("DB_HOST", "ymysql-1ce5eb8-jose-eae6.e.aivencloud.com")
     DB_USER = os.getenv("DB_USER", "avnadmin")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "AVNS_u4xv4qhGhPxOY13Gm-L")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "PWD")
     DB_NAME = os.getenv("DB_NAME", "globant_challenge")
     DB_PORT = int(os.getenv("DB_PORT", 20906))  
     DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4") 
