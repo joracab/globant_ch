067c741f85a7082628fd97bf639440771706f025
joracab <jose.l.ramirez@gmail.com>
2025-02-21 13:56:15 -0500

    update naming and flask structure, add jobs logic

--- API/config.py
+++ API/config.py
@@ -1,9 +1,13 @@
 import os
 
+import os
+
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
-    DB_HOST = os.getenv("DB_HOST", "your_host")
-    DB_USER = os.getenv("DB_USER", "your_user")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "your_password")
-    DB_NAME = os.getenv("DB_NAME", "your_database")
+    DB_HOST = os.getenv("DB_HOST", "ymysql-1ce5eb8-jose-eae6.e.aivencloud.com")
+    DB_USER = os.getenv("DB_USER", "avnadmin")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "AVNS_u4xv4qhGhPxOY13Gm-L")
+    DB_NAME = os.getenv("DB_NAME", "globant_challenge")
+    DB_PORT = int(os.getenv("DB_PORT", 20906))  
+    DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4") 
     DB_CURSORCLASS = "DictCursor"
+
